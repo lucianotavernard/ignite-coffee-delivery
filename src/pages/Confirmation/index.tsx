8c171a79dc8e1f6597db56bb0b6d4c9c0b6b6056
Luciano Tavernard <lucianotavernard@gmail.com>
2022-08-01 22:49:22 -0300

    refactor: adjust confirmation page

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -50,48 +50,53 @@ export function Confirmation() {
               </hgroup>
 
               <div className="flex flex-col p-[0.0625rem] rounded-tl-md rounded-tr-[2rem] rounded-br-md rounded-bl-[2rem] bg-gradient-to-tr from-amber-500 to-fuchsia-700">
-                <ul className="grid grid-cols-1 gap-6 p-10 rounded-tl-md rounded-tr-[2rem] rounded-br-md rounded-bl-[2rem] font-roboto font-normal text-base bg-[#FAFAFA] text-[#574F4D]">
-                  <li className="flex items-center">
-                    <span className="flex justify-center items-center w-8 h-8 mr-3 rounded-full bg-[#8047F8] text-base text-white">
-                      <MapPin />
-                    </span>
+                {location && (
+                  <ul className="grid grid-cols-1 gap-6 p-10 rounded-tl-md rounded-tr-[2rem] rounded-br-md rounded-bl-[2rem] font-roboto font-normal text-base bg-[#FAFAFA] text-[#574F4D]">
+                    <li className="flex items-center">
+                      <span className="flex justify-center items-center w-8 h-8 mr-3 rounded-full bg-[#8047F8] text-base text-white">
+                        <MapPin />
+                      </span>
 
-                    <hgroup>
-                      <p className="text-left">
-                        Entrega em{' '}
+                      <hgroup>
+                        <p className="text-left">
+                          Entrega em{' '}
+                          <strong>
+                            {location.street}, {location.number}
+                          </strong>
+                        </p>
+
+                        <p>
+                          {location.neighborhood} - {location.city},{' '}
+                          {location.uf}
+                        </p>
+                      </hgroup>
+                    </li>
+
+                    <li className="flex items-center">
+                      <span className="flex justify-center items-center w-8 h-8 mr-3 rounded-full bg-[#DBAC2C] text-base text-white">
+                        <Timer />
+                      </span>
+
+                      <hgroup>
+                        <p className="text-left">Previsão de entrega</p>
+                        <strong>20 min - 30 min</strong>
+                      </hgroup>
+                    </li>
+
+                    <li className="flex items-center">
+                      <span className="flex justify-center items-center w-8 h-8 mr-3 rounded-full bg-[#C47F17] text-base text-white">
+                        <CurrencyDollar />
+                      </span>
+
+                      <hgroup>
+                        <p className="text-left">Pagamento na entrega</p>
                         <strong>
-                          {location.street}, {location.number}
+                          {paymentMethods[location.paymentMethod]}
                         </strong>
-                      </p>
-
-                      <p>
-                        {location.neighborhood} - {location.city}, {location.uf}
-                      </p>
-                    </hgroup>
-                  </li>
-
-                  <li className="flex items-center">
-                    <span className="flex justify-center items-center w-8 h-8 mr-3 rounded-full bg-[#DBAC2C] text-base text-white">
-                      <Timer />
-                    </span>
-
-                    <hgroup>
-                      <p className="text-left">Previsão de entrega</p>
-                      <strong>20 min - 30 min</strong>
-                    </hgroup>
-                  </li>
-
-                  <li className="flex items-center">
-                    <span className="flex justify-center items-center w-8 h-8 mr-3 rounded-full bg-[#C47F17] text-base text-white">
-                      <CurrencyDollar />
-                    </span>
-
-                    <hgroup>
-                      <p className="text-left">Pagamento na entrega</p>
-                      <strong>{paymentMethods[location.paymentMethod]}</strong>
-                    </hgroup>
-                  </li>
-                </ul>
+                      </hgroup>
+                    </li>
+                  </ul>
+                )}
               </div>
             </aside>
 
